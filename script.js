79fce69c15e56739b245cb468e7c2f097cc014d9
Vinh <ashawanawompwompmeow@gmail.com>
2024-08-13 21:57:41 -0500

    Well Yes! (Worked w/ Matching, Sounds, Timer, Flip Counter)

--- script.js
+++ script.js
@@ -1,128 +1,116 @@
 class audioController {
     constructor() {
         this.flipSound = new Audio('Assets/Audio/Flip.mp3');
-        this.matchedSound = new Audio('Assets/Audio/matched.mp3')
+        this.matchedSoundPLACEHOLDER = new Audio('Assets/Audio/placeholderMatched.mp3'); //change
     }
-    flip() {
-        this.flipSound.play();
+    matchedPLACEHOLDER() {
+        this.matchedSoundPLACEHOLDER.play(); //change
     }
-    matched() {
-        this.matchedSound.play();
+    playFlipSound() {
+        this.flipSound.play(); //adjust audio volume?
     }
 }
 
 class gameController {
-    constructor(totalTime, cards) { //basically just parameters
-        this.cardArray = cards;
-        this.totalTime = totalTime; //setting a this to have a constant time
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remaining'); //so we can change it in startCountdown()
-        this.numFlips = document.getElementById('flips');
-        this.audio = new audioController();
-
+    constructor(totalTime, cards) {
+        this.constantTime = totalTime;
+        this.getTime = document.getElementById('time-remaining')
+        this.getFlips = document.getElementById('flips');
+        this.cardsArray = cards;
+        this.music = new audioController();
     }
 
     startGame() {
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime; // resetting the timeRemaining with the constant this.totalTime representing the total time
-        this.timer.innerText = this.totalTime;
-        this.numFlips.innerText = this.totalClicks;
+        this.getTime.innerText = this.constantTime;
+        this.timeRemaining = this.constantTime;
         this.cardToCheck = null;
-        this.matchedCards = [];
-        this.shuffleCards();
-        this.hideCards(); //to revert the cards back to invisible
-        this.countDown = this.startCountdown();
+        this.cardsMatchedArray = [];
+        this.numFlips = 0; //resets
+        this.startCountdown();
+        
+    }
+
+    flipCard(card) {
+        this.music.playFlipSound();
+        card.classList.add('visible');
+        this.numFlips++; 
+        this.getFlips.innerText = this.numFlips;
+
+
+        if (this.cardToCheck) {
+            this.checkForCardMatch(card);
+        } else {
+            this.cardToCheck = card;
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+
+    gameVictory() {
+        //needs a wait time so when the video ends it will be visible
+        document.getElementById('victory').classList.add('visible');
     }
 
     gameOver() {
-        clearInterval(this.countDown);
-        document.querySelector('.gameover').classList.add('visible');
+        document.getElementById('gameover').classList.add('visible');
     }
 
-    victory() {
-        clearInterval(this.countDown);
-        document.querySelector('.victory').classList.add('visible');
-    }
+    //---------------------------------------------------------------------------------------------------------
 
     startCountdown() {
-        return setInterval(() => {
+        this.countDown = setInterval(() => { //setInterval returns an ID but without anything to return it cant work with clearInterval
             this.timeRemaining--;
-            this.timer.innerText = this.timeRemaining;
+            this.getTime.innerText = this.timeRemaining;
             if (this.timeRemaining === 0) {
+                this.stopCountdown();
                 this.gameOver();
             }
         }, 1000);
     }
 
-    hideCards() {
-        this.cardArray.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.remove('matched'); //remove all the matched classes since we wanted to place the video when they matched
-        })
+    stopCountdown() {
+        clearInterval(this.countDown);
     }
 
-    flipCard(card) {
-        if (this.canFlipCard(card)) {
-            this.audio.flip();
-            this.totalClicks++;
-            this.numFlips.innerHTML = this.totalClicks;
-            card.classList.add('visible'); // needs to be in here to be able to have a conditional statement on whether or no the card is allowed to be flipped
-        }
+    //---------------------------------------------------------------------------------------------------------
 
-        if(this.cardToCheck) { //if its null it will go to else, if its true it will go to checkForCardMatch()
-            this.checkForCardMatch(card); //if there is a card selected, it will check to see if matched
-        } else {
-            this.cardToCheck = card; // else the card selected will be considered the selected card
-        }
+    cardsMatched(card1, card2) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
 
+        card1.getElementsByClassName('video')[0].play();
+        card2.getElementsByClassName('video')[0].play();
+
+        this.cardsMatchedArray.push(card1);
+        this.cardsMatchedArray.push(card2);
+        if (this.cardsMatchedArray.length === this.cardsArray.length) {
+            this.gameVictory();
+        }
+    }
+
+    cardsNotMatched() {
+        //Work On This
     }
 
     checkForCardMatch(card) {
-        if (this.getCardType(card) === this.getCardType(this.getCardType(this.cardToCheck))) {
-            this.cardMatch(card, this.cardToCheck);
+        if (this.getType(card) == this.getType(this.cardToCheck)) {
+            this.music.matchedPLACEHOLDER(); //CHANGE AND ALSO HOW TO ADD A DELAY SO THAT IT PLAYS WHEN CARD IS FULLY FLIPPED
+            this.cardsMatched(card, this.cardToCheck);
         } else {
-            this.cardMismatch(card, this.cardToCheck);
+            this.cardsNotMatched();
         }
+        this.cardToCheck = null;
     }
 
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        this.audio.matchedSound();
-        if (this.matchedCards.length === this.cardArray.length) {
-            this.victory();
-        }
+    getType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
     }
-
-    cardMismatch(card) {
-
-    }
-
-    getCardType(card) {
-        return card.getElementsByClassName('card-value')[0].src;//since its not an array but just an individual image with a class of "card-value"
-    }
-
-    canFlipCard(card) {
-        return true; //temporary for now
-    }
-
-    shuffleCards() {
-        for (let i = this.cardArray.length - 1; i > 0; i--) { // example if i = 4, math.random * 5 = [0, 5), truncate anything in 4.0 gets us 4
-            let randIndex = Math.floor(Math.random() * (i + 1)); // basically stretching the range from [0, 5) then truncating anything within 4 point something to only get 4
-            this.cardArray[randIndex].style.order = i;
-            this.cardArray[i].style.order 
-        }
-    }
-
 }
+
 
 let overlays = Array.from(document.getElementsByClassName('overlay-text'));
 let cards = Array.from(document.getElementsByClassName('card'));
-let game = new gameController(5, cards);
-
-
+let game = new gameController(1000, cards);
 
 overlays.forEach(overlay => {
     overlay.addEventListener('click', () => {
